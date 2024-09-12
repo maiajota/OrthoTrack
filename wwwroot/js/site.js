68a26e1680a406d5ee983cc6a2cffda75901a8b7
João Victor Maia <maia.joaovic@gmail.com>
2024-09-12 12:07:00 -0300

    feat: correção do formulário

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,15 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-$('tr[data-href]').on('click', (row) => {
+﻿$('tr[data-href]').on('click', (row) => {
     let href = row.currentTarget.getAttribute('data-href');
     window.location.href = href
 })
+
+function getTodayDate(){
+    const date = new Date();
+    const dateDay = date.getDate();
+    const dateMonth = date.getMonth() + 1;
+    const dateYear = date.getFullYear();
+
+    return `${dateYear}-${'0' + dateMonth}-${dateDay}`;
+}
+
+document.querySelectorAll('input[type=date]')[1].value = getTodayDate()
